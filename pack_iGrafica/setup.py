47bf4a74222b1c416f2783eb0aada5988f0b707f
Marcelo Mareco <marcelo12mm@gmail.com>
2020-08-06 21:29:46 -0300

    En setup.py se crean los features del programa (información)

---
+++ pack_iGrafica/setup.py
@@ -0,0 +1,14 @@
+"""info del pack"""
+
+from setuptools import setup
+
+setup(
+	name = "iGrafica",
+	version = "1.0",
+	author = "Marcelo Mareco",
+	author_email = "Programmarce@gmail.com",
+	license = "GNU",
+	url = "#",
+	packages = ["pack_iGrafica"]
+
+	)
